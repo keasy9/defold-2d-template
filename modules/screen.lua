fa7480999944a16ddf709c3e44ac3f1f4a70b89b
keasy9 <keasy9@gmail.com>
2024-11-15 22:16:04 +0700

    загрузка при смене экрана

--- modules/screen.lua
+++ modules/screen.lua
@@ -1,7 +1,21 @@
 local M = {};
 
-function M.show(proxy)
+function M.show(self, proxy)
 	msg.post('main:/controller#main', 'load_proxy', {proxy = proxy})
+
+	return self
+end
+
+function M.loader(self, state)
+	msg.post('main:/controller#loader', state and 'start' or 'stop')
+
+	return self
+end
+
+function M.unload(self)
+	msg.post('main:/controller#main', 'unload_proxy')
+
+	return self
 end
 
 return M;
